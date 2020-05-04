1e51212be8320bddbaa4471e7488d3696b3b6802
Jacob Lever <jacobianism@gmail.com>
2020-05-04 20:46:22 +0100

    Add WebSocket backend using API Gateway and AWS Lambda

---
+++ sendmessage/app.js
@@ -0,0 +1,50 @@
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+const AWS = require('aws-sdk');
+
+const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
+
+const { TABLE_NAME } = process.env;
+
+exports.handler = async event => {
+  let connectionData;
+  
+  try {
+    connectionData = await ddb.scan({ TableName: TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
+  } catch (e) {
+    return { statusCode: 500, body: e.stack };
+  }
+  
+  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+    apiVersion: '2018-11-29',
+    endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
+  });
+  
+  const postData = JSON.parse(event.body).data;
+  
+  const postCalls = connectionData.Items
+    .filter(connectionId => connectionId !== event.requestContext.connectionId)
+    .map(async ({ connectionId }) => {
+      try {
+        if (connectionId !== event.requestContext.connectionId) {
+          await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
+        }
+      } catch (e) {
+        if (e.statusCode === 410) {
+          console.log(`Found stale connection, deleting ${connectionId}`);
+          await ddb.delete({ TableName: TABLE_NAME, Key: { connectionId } }).promise();
+        } else {
+          throw e;
+        }
+      }
+    });
+  
+  try {
+    await Promise.all(postCalls);
+  } catch (e) {
+    return { statusCode: 500, body: e.stack };
+  }
+
+  return { statusCode: 200, body: 'Data sent.' };
+};
