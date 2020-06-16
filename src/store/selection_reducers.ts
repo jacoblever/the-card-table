2a707e3d0882790394795d3bd16f87aa9c967312
Jacob Lever <jacobianism@gmail.com>
2020-06-16 01:37:08 +0100

    Improve the selection algo, so that all _nearby_ cards are selected

--- src/store/selection_reducers.ts
+++ src/store/selection_reducers.ts
@@ -1,5 +1,6 @@
-import { CardState } from "./state";
+import { Card, CardState } from "./state";
 import { SelectCardsUnderAction } from "./actions";
+import { Elementwise } from "../geometry/elementwise"
 
 export function SelectCardsUnderReducer(
   state: CardState,
@@ -9,20 +10,60 @@ export function SelectCardsUnderReducer(
   const cardHeight = 86;
   let cardClicked = state.cardsById[action.cardId];
   let allCards = Object.values(state.cardsById);
-  let cardsToUnderThisOne = allCards
-    .filter(x => x.heldBy === cardClicked.heldBy)
-    .filter(x => x.zIndex <= cardClicked.zIndex)
-    .filter(x => Math.abs(x.location[0] - cardClicked.location[0]) < cardWidth)
-    .filter(x => Math.abs(x.location[1] - cardClicked.location[1]) < cardHeight)
-    .sort((a, b) => b.zIndex - a.zIndex);
+  let cardsWithSameOwner = allCards
+    .filter(x => x.heldBy === cardClicked.heldBy);
+
+  let cardsToSelect: Card[] = [];
+  let selectedRegionTopLeft = cardClicked.location;
+  let selectedRegionBottomRight = cardClicked.location;
+  while (true) {
+    let addedCount = 0;
+    let cardsToCheck = cardsWithSameOwner.filter(x => !cardsToSelect.map(y => y.id).includes(x.id));
+    for (let i = 0; i < cardsToCheck.length; i++) {
+      let card = cardsToCheck[i];
+
+      let isCloseToTopLeft = Math.abs(card.location[0] - selectedRegionTopLeft[0]) < cardWidth / 2
+        && Math.abs(card.location[1] - selectedRegionTopLeft[1]) < cardHeight / 2;
+      let isCloseToBottomRight = Math.abs(card.location[0] - selectedRegionBottomRight[0]) < cardWidth / 2
+        && Math.abs(card.location[1] - selectedRegionBottomRight[1]) < cardHeight / 2;
+      if (isCloseToTopLeft || isCloseToBottomRight) {
+        addedCount++;
+        cardsToSelect.push(card);
+      }
+    }
+    if (addedCount === 0) {
+      break;
+    }
+
+    selectedRegionTopLeft = Elementwise.map(i => {
+      return Math.min(...cardsToSelect.map(x => x.location[i]));
+    });
+    selectedRegionBottomRight = Elementwise.map(i => {
+      return Math.max(...cardsToSelect.map(x => x.location[i]));
+    });
+  }
+
+  cardsToSelect.sort((a, b) => b.zIndex - a.zIndex);
 
   // If we have already selected some cards we should select everything rather than
   // just the cards in the same faceUp state as the top one
   let selectAll = cardClicked.selected;
 
+  let clickedIndex = cardsToSelect.map(x => x.id).indexOf(cardClicked.id);
   let newState: CardState = {...state};
-  for(let i = 0; i < cardsToUnderThisOne.length; i++) {
-    let card = cardsToUnderThisOne[i];
+  newState.cardsById[cardClicked.id] = {...cardClicked, selected: true};
+
+  for(let i = clickedIndex + 1; i < cardsToSelect.length; i++) {
+    let card = cardsToSelect[i];
+    if(!selectAll && card.faceUp !== cardClicked.faceUp) {
+      break;
+    }
+
+    newState.cardsById[card.id] = {...card, selected: true};
+  }
+
+  for(let i = clickedIndex - 1; i >= 0; i--) {
+    let card = cardsToSelect[i];
     if(!selectAll && card.faceUp !== cardClicked.faceUp) {
       break;
     }
